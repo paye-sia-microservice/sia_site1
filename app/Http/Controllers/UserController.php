54b72ee59d043fe8138c9a8f2100c1f1a75ee38a
PayeJohn2022 <114854688+PayeJohn2022@users.noreply.github.com>
2023-05-02 06:23:26 +0800

    First Push to Repository

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use App\Traits\ApiResponser;
 use DB;
 
 Class UserController extends Controller {
-    use ApiResponser; // <- successResponse
+    use ApiResponser; 
 
     private $request;
 
@@ -20,17 +20,12 @@ Class UserController extends Controller {
     public function getUsers(){    
         // $users = User::all(); // eloquent style
 
-        $users = DB::connection('mysql')     // using raw SQL queries 
-        ->select("Select * from tbluser");
+        // $users = DB::connection('mysql')     // using raw SQL queries 
+        // ->select("Select * from tbluser");
 
-        // return response()->json(['data' => $users], 200); // old and redundant code
+        //return response()->json(['data' => $users], 200); 
         return $this->successResponse($users);
     }
-
-    public function index(){
-        $users = User::all();
-        return $this->successResponse($users);
-    }   
 
     public function addUser(Request $request ){ 
         $rules = [
@@ -56,35 +51,5 @@ Class UserController extends Controller {
         {
             return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
         }
-    }
-
-    public function updateUser(Request $request, $id) { 
-        $rules = [
-            'username' => 'required|max:20',
-            'password' => 'required|max:20',
-            'gender' => 'required|in:Male,Female'
-        ];
-    
-        $this->validate($request, $rules);
-    
-        $user = User::findOrFail($id);
-    
-        $user->fill($request->all());
-    
-        if ($user->isClean()) {
-            return Response()->json("At least one value must change", 
-            Response::HTTP_UNPROCESSABLE_ENTITY);
-        } else {
-            $user->save();
-            return $this->successResponse($user);
-        }
-    }
-
-    public function deleteUser($id) { 
-        $user = User::findOrFail($id);
-    
-        $user->delete();
-    
-        return $this->successResponse($user);
     }
 }
