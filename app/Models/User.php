bcc40ed3605bd4c1aa074e11a18f83b19ff9bdfc
PayeJohn2023 <payejohndwight@gmail.com>
2023-06-07 13:00:58 +0800

    Lab 3 & 4 Activity Finals

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,18 @@
 
 namespace App\Models;
 
-use App\Models\User;
-
 use Illuminate\Database\Eloquent\Model;
-
 
 class User extends Model {
 
-    public $timestamps = false;
-
     protected $table = 'tbluser';
-    protected $primaryKey = 'userid';
+        protected $fillable = [
+            'username','password','gender','jobid'
+        ];
 
-    protected $fillable = [
-        'username', 'password', 'gender', 'jobid'
-    ];
-
-    protected $hidden =[
-        "password"
-    ];
+        public $timestamps = false;
+        protected $primaryKey = 'userid';
+        protected $hidden = [
+            'password',
+        ];
 }
